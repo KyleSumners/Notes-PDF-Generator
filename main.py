1c4a1fc99d4163d11756cbbd534bb6079a6ed8eb
KyleSumners <114194918+KyleSumners@users.noreply.github.com>
2024-05-03 00:33:44 -0500

    Add creation of a single page pdf

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ pdf = FPDF(orientation="P", unit="mm")
 
 pdf.add_page()
 
+pdf.set_font(family="Times", style="B", size=12)
+pdf.cell(w=0, h=12, txt="Hello There!", align="L", ln=1, border=0)
+
 pdf.output("output.pdf")
