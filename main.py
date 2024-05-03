9d5cb118d49c5df1746596eeb1bb1dea79e6b43f
KyleSumners <114194918+KyleSumners@users.noreply.github.com>
2024-05-03 01:09:17 -0500

    Add lines to pages for notes

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 21, 200, 21)
+
+    # Add lines
+    for i in range(20, 297, 10):
+        pdf.line(10, i, 200, i)
 
     # Set the footer
     pdf.ln(265)
@@ -22,6 +25,10 @@ for index, row in df.iterrows():
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+
+        # Add lines
+        for i in range(20, 298, 10):
+            pdf.line(10, i, 200, i)
 
         # Set the footer
         pdf.ln(277)
